01a46277510185b6d70afb4e13f031f7f7488d19
jay <jeanalainpro2@gmail.com>
2024-04-29 13:08:10 +0200

    cicd ends

--- kind/create_kind_cluster.sh
+++ kind/create_kind_cluster.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
+cd ..
 echo "------------------------"
 echo "K8s Cluster init ..."
 kind create cluster --name insight-hoot --config ./kind/k8s_config/kind-config.yaml
-echo "Deploying Zookeeper ..."
-kubectl apply -f k8s_config/kafka/zookeeper.yaml
-echo "Deploying Kafka ..."
-kubectl apply -f k8s_config/kafka/kafka.yaml
+echo "Deploying Default Namespace: Zookeeper, Kafka, Kafka Connect, Kafka UI ..."
+kubectl apply -f ./kind/k8s_config/kafka/
 
 echo "------------------------"
 echo "Building images producer"
@@ -17,28 +16,20 @@ echo "Pass"
 # kind load docker-image -n insight-hoot producer-kafka producer-kafka
 # cd ..
 
-
-echo "------------------------"
-echo "Deploying Kafka-producer.yaml ..."
-echo "Pass"
-# kubectl apply -f k8s_config/kafka/kafka-producer.yaml
-
-echo "------------------------"
-echo "Put Spark image in cluster"
-kind load docker-image -n insight-hoot spark:our-own-apache-spark-kb8
-kubectl apply -f k8s_config/kafka/spark-sa.yaml
+# echo "------------------------"
+# echo "Put Spark image in cluster"
+# kind load docker-image -n insight-hoot spark:our-own-apache-spark-kb8
+# kubectl apply -f k8s_config/kafka/spark-sa.yaml
 
 
 echo "------------------------"
 echo "Install helm Chart Spark operator"
 echo "Pass"
-# helm repo add spark-operator https://kubeflow.github.io/spark-operator
-# helm install my-release spark-operator/spark-operator --namespace spark-operator --create-namespace --set webhook.enable=true
-#
-echo "------------------------"
-echo "Kafka Connect Deployment"
-kubectl apply -f k8s_config/kafka/kafka-connect.yaml
+helm repo add spark-operator https://kubeflow.github.io/spark-operator
+helm install my-release spark-operator/spark-operator --namespace spark-operator --create-namespace --set webhook.enable=true
+
+
 
 echo "------------------------"
-echo "Kafka UI Deployment"
-kubectl apply -f k8s_config/kafka/kafka-ui.yaml
+echo "Spark Application"
+kubectl apply -f ./kind/k8s_config/spark-pi.yaml
