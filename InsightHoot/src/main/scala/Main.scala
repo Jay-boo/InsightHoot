55cfc151738e9d5b76c96bb4f0b9dc6295277954
jay <jeanalainpro2@gmail.com>
2024-04-30 16:27:00 +0200

    Scala code Get yesterday messages

--- InsightHoot/src/main/scala/Main.scala
+++ InsightHoot/src/main/scala/Main.scala
@@ -1,51 +1,57 @@
 import org.apache.spark.sql.SparkSession;
-import org.apache.spark.sql.Encoders;
+import org.apache.spark.sql.{Encoders};
 
 object Main {
   def main(args: Array[String]): Unit = {
-    // Create SparkSession
+    if (args.length != 1 || (args(0) != "local" && args(0) != "k8s")) {
+      println("Usage: Main <mode>")
+      println("Modes: local | k8s")
+      System.exit(1)
+    }
+    val mode: Boolean = args(0) == "k8s"
+    val kafkaParams = Map[String, String](
+      "kafka.bootstrap.servers" ->( if (mode) "kafka-service.default.svc.cluster.local:9092" else  "localhost:9092"),
+      "subscribe" -> "databricks",
+      "startingOffsets"-> "earliest",
+      "endingOffsets"-> "latest"
+    )
+    println(s"Mode : $mode")
+    println(kafkaParams)
+
     val spark = SparkSession
       .builder()
       .appName("KafkaToSparkStreaming")
       .config("spark.master","local")
       .getOrCreate()
 
-    // Define Kafka parameters
-    val kafkaParams = Map[String, String](
-      "kafka.bootstrap.servers" -> "kafka-service.default.svc.cluster.local:9092", // Kafka broker
-      "subscribe" -> "databricktopic" // Kafka topic to subscribe to
-      //"auto.offset.reset" -> "latest" // Read from the latest offset
-    )
-
-    // Define Kafka topic to subscribe to
-    val kafkaTopic = "databricktopic"
-
-    // Read data from Kafka as a DataFrame
-    val kafkaStreamDF = spark
-      .readStream
+    val df=spark
+      .read
       .format("kafka")
       .options(kafkaParams)
       .load()
 
-    // Define value schema
-    val valueSchema = "value STRING"
+    println("Getting DF from kafka")
+    df.printSchema()
+    df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)").show(4,truncate=false)
 
-    // Parse the value from Kafka message into a DataFrame
-    val parsedDF = kafkaStreamDF
-      .selectExpr("CAST(value AS STRING)")
-      .selectExpr(valueSchema)
-      .as(Encoders.STRING)
-
-    // Your processing logic here
-    // For example, you can write the DataFrame to console
-
-    val query = parsedDF
-      .writeStream
-      .outputMode("append")
-      .format("console")
-      .start()
-
-    // Wait for the termination of the query
-    query.awaitTermination()
+//    val kafkaStreamDF = spark
+//      .readStream
+//      .format("kafka")
+//      .options(kafkaParams)
+//      .load()
+//
+//    val valueSchema = "value STRING"
+//    val parsedDF = kafkaStreamDF
+//      .selectExpr("CAST(value AS STRING)")
+//      .selectExpr(valueSchema)
+//      .as(Encoders.STRING)
+//
+//
+//    val query = parsedDF
+//      .writeStream
+//      .outputMode("append")
+//      .format("console")
+//      .start()
+//    query.awaitTermination()
   }
 }
