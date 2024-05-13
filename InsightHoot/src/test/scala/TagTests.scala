818002b4ef025526f754e6f197693b51aa37ca38
jay <jeanalainpro2@gmail.com>
2024-05-13 14:33:44 +0200

    refactor test

--- InsightHoot/src/test/scala/TagTests.scala
+++ InsightHoot/src/test/scala/TagTests.scala
@@ -2,16 +2,22 @@ import org.apache.spark.sql.DataFrame
 
 import scala.io.Source
 
-class MyTests extends munit.FunSuite with SparkMachine {
+class TagTests extends munit.FunSuite with SparkMachine {
   test("sum of two integers") {
     val obtained = 2 + 2
     val expected = 4
     assertEquals(obtained, expected)
   }
 
-  test("getPayload test"){
+  test("getPayload() test"){
     val df: DataFrame=spark.read.parquet("src/test/resources/raw_data/topic_name=4sysops/4sysops_02052024.parquet")
     df.show()
     val payloadDF:DataFrame=Main.getPayload(df)
   }
+
+
+  test("tagDF() test"){
+
+  }
+
 }
