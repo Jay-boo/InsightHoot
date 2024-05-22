3ba2f010f1d49d82360a7fd058f63eea33341312
jay <jeanalainpro2@gmail.com>
2024-05-22 11:46:20 +0200

    [Fix CI/CD] fixed using chart v1.2.7

--- kind/create_kind_cluster.sh
+++ kind/create_kind_cluster.sh
@@ -26,10 +26,10 @@ echo "------------------------"
 echo "Install helm Chart Spark operator"
 echo "Pass"
 helm repo add spark-operator https://kubeflow.github.io/spark-operator
-helm install my-release spark-operator/spark-operator --namespace spark-operator --create-namespace --set webhook.enable=true --debug
-#
-#
-#
-# echo "------------------------"
-# echo "Spark Application"
-# kubectl apply -f ./kind/k8s_config/spark-pi.yaml
+helm install my-release spark-operator/spark-operator --version 1.2.7 --namespace spark-operator --create-namespace --set webhook.enable=true --debug
+
+
+
+echo "------------------------"
+echo "Spark Application"
+kubectl apply -f ./kind/k8s_config/spark-pi.yaml
