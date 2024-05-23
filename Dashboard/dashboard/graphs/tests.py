d24b97309e00054686303dd78373694b33197e66
freddytiroumalai <freddy.tiroumalai@datafab.io>
2024-05-23 11:52:59 +0200

    updated test for secure

--- Dashboard/dashboard/graphs/tests.py
+++ Dashboard/dashboard/graphs/tests.py
@@ -3,13 +3,17 @@ from django.core.management import call_command
 from .models import Messages, Topics, Tags, MessageTags
 from django.urls import reverse
 from django.utils import timezone
+from django.test import Client
+from django.contrib.auth.models import User
 
 class MessagesTestCase(TestCase):
     
-    databases = {'feeds'}
+    databases = {'default', 'feeds'}
 
 
     def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user("dio")
         Topics.objects.create(name="testtopic", url="dummyurl")
         Tags.objects.create(label="testlabel", theme="testtheme")
         
@@ -22,7 +26,8 @@ class MessagesTestCase(TestCase):
         self.assertEqual(message_test.topic, topic_test)
         self.assertIsInstance(message_test, Messages)
 
-    def test_messages_by_topic(self):
+    def test_messages_by_topic_logged_in(self):
+        self.client.force_login(self.user)
         topic_test = Topics.objects.get(name="testtopic")
         tag_test = Tags.objects.get(label="testlabel", theme="testtheme")
         Messages.objects.create(date=timezone.now() ,content="This is a test content", title="Test Title", link="https://example.com", topic=topic_test)
