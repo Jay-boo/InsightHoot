095949910f16c821eb9129075113d42f16d2e5d8
jay <jeanalainpro2@gmail.com>
2024-05-31 10:24:00 +0200

    [CICD] Launch psql djngo service and app + check pods status

--- kind/create_kind_cluster.sh
+++ kind/create_kind_cluster.sh
@@ -6,21 +6,11 @@ echo "K8s Cluster init ..."
 kind create cluster --name insight-hoot --config ./kind/k8s_config/kind-config.yaml
 echo "Deploying Default Namespace: Zookeeper, Kafka, Kafka Connect, Kafka UI ..."
 kubectl apply -f ./kind/k8s_config/kafka/
+kubectl apply -f ./kind/k8s_config/django/
 
 echo "------------------------"
 echo "Building images producer"
 echo "Pass"
-# cd producer-img
-# docker build -t producer-kafka .
-# echo "Push Image to cluster"
-# kind load docker-image -n insight-hoot producer-kafka producer-kafka
-# cd ..
-
-# echo "------------------------"
-# echo "Put Spark image in cluster"
-# kind load docker-image -n insight-hoot spark:our-own-apache-spark-kb8
-# kubectl apply -f k8s_config/kafka/spark-sa.yaml
-
 
 echo "------------------------"
 echo "Install helm Chart Spark operator"
