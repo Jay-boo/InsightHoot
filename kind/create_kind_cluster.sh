d990df36a7501d62ba7667d40af14b5549650cd3
Jean-Alain Morel <81867817+Jay-boo@users.noreply.github.com>
2024-06-03 12:14:08 +0200

    [CICD] Launch psql django service and app + check pods status (#66)
    * [CICD] Launch psql djngo service and app + check pods status
    
    * fix kubectl comman in workflow yaml
    
    * [CICD + k8s] Setup ingress nginx controller to check status of django app
    
    * [CICD + k8s] Setup ingress nginx controller to check status of django app
    
    * [CICD + k8s] Setup ingress nginx controller to check status of django app
    
    * [CICD + k8s] Setup ingress nginx controller to check status of django app
    
    * [CICD + k8s] Setup ingress nginx controller to check status of django app
    
    * [CICD + k8s] Setup ingress nginx controller to check status of django app
    
    * [CICD + k8s] Setup ingress nginx controller to check status of django app
    
    * [CICD] fix check status of django app
    
    * [CICD] fix check status of django app

--- kind/create_kind_cluster.sh
+++ kind/create_kind_cluster.sh
@@ -8,28 +8,17 @@ echo "Deploying Default Namespace: Zookeeper, Kafka, Kafka Connect, Kafka UI ...
 kubectl apply -f ./kind/k8s_config/kafka/
 
 echo "------------------------"
-echo "Building images producer"
-echo "Pass"
-# cd producer-img
-# docker build -t producer-kafka .
-# echo "Push Image to cluster"
-# kind load docker-image -n insight-hoot producer-kafka producer-kafka
-# cd ..
-
-# echo "------------------------"
-# echo "Put Spark image in cluster"
-# kind load docker-image -n insight-hoot spark:our-own-apache-spark-kb8
-# kubectl apply -f k8s_config/kafka/spark-sa.yaml
-
-
-echo "------------------------"
 echo "Install helm Chart Spark operator"
 echo "Pass"
 helm repo add spark-operator https://kubeflow.github.io/spark-operator
 helm install my-release spark-operator/spark-operator --version 1.2.7 --namespace spark-operator --create-namespace --set webhook.enable=true --debug
 
-
-
 echo "------------------------"
 echo "Spark Application"
 kubectl apply -f ./kind/k8s_config/spark-pi.yaml
+
+
+kubectl apply -f  https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
+kubectl apply -f ./kind/k8s_config/django/
+sleep 60
+kubectl apply -f ./kind/k8s_config/ingress.yaml
